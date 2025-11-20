7fd4934a164fe8f3e80af2782dbe9cef07388b9d
Anna Liu <liuanna0918@gmail.com>
2025-11-19 21:46:58 -0500

    flash experiments

--- feather_sense_pio_project/src/ble_manager.h
+++ feather_sense_pio_project/src/ble_manager.h
@@ -11,16 +11,11 @@ extern "C" {
 }
 #endif
 
-
-// BLE Service
-extern BLEDfu  bledfu;  // OTA DFU service
-extern BLEDis  bledis;  // device information
-extern BLEUart bleuart; // uart over ble
-extern BLEBas  blebas;  // battery 
+void my_connect_cb();
+void my_disconnect_cb();
 
 void ble_init(void);
 void startAdv(void);
-void ble_write(char *buf, uint16_t size);
 void ble_write_bytes_chunked(const uint8_t *data, size_t len);
 void print_both(const char *fmt, ...);
 
